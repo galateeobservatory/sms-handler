5c9375f972ef34c2252fc8ec739dc2b0771ce37d
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-09-17 22:15:12 +0200

    Can get CSRF token

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 use std::io::Read;
-use error_chain::error_chain;
-
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
-}
+use regex::Regex;
 
 fn main() {
-    let mut res = reqwest::blocking::get("http://meayne.ddns.net").unwrap();
+    // let mut res = reqwest::blocking::get("http://192.168.8.1/api/sms/sms-list").unwrap();
+    let mut res =reqwest::blocking::get("http://192.168.8.1/html/smsinbox.html?smssent").unwrap();
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
+    //println!("Status: {}", res.status());
+    //println!("Headers:\n{:#?}", res.headers());
+    //println!("Body:\n{}", body);
+    println!("{}", get_csrf_token(&body));
 
+}
+
+fn get_csrf_token(body: &String) -> String {
+    let re = Regex::new(r#"<meta name="csrf_token" content="(.*)"/>"#).unwrap();
+    let mut token: String = String::from("");
+    for line in body.lines() {
+        if re.is_match(line) {
+            let tokens = re.captures(line).unwrap();
+            if tokens.len() >= 2 {
+                token = String::from(&tokens[1]);
+            }
+        }
+    }
+
+    return String::from(token);
 }
