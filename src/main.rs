4710c62cc7868ff7ba7fca53e334dac9e491c3fa
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-09-25 10:53:28 +0200

    Can get SMS

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,31 @@ use std::io::Read;
 use regex::Regex;
 
 fn main() {
-    // let mut res = reqwest::blocking::get("http://192.168.8.1/api/sms/sms-list").unwrap();
-    let mut res =reqwest::blocking::get("http://192.168.8.1/html/smsinbox.html?smssent").unwrap();
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
+    let mut builder = reqwest::blocking::ClientBuilder::new();
+    builder = builder.cookie_store(true);
+    let client = builder.build().unwrap();
+    //let client = reqwest::blocking::Client::new();
+    let mut res = client.get("http://192.168.8.1/html/smsinbox.html?smssent").send().unwrap();
 
-    //println!("Status: {}", res.status());
-    //println!("Headers:\n{:#?}", res.headers());
-    //println!("Body:\n{}", body);
-    println!("{}", get_csrf_token(&body));
+    let mut body = String::new();
+    {
+        res.read_to_string(&mut body).unwrap();
+    }
+    {
+        let c = res.cookies().next().unwrap();
+        println!("{} = {}", c.name(), c.value());
+    }
+
+    let csrf_token = get_csrf_token(&body);
+    println!("{}", csrf_token);
+    let CUSTOM_HEADER: &'static str = "__RequestVerificationToken";
+    // boxtype = 1 -> recv
+    // boxtype = 2 -> sent
+    res = client.post("http://192.168.8.1/api/sms/sms-list").header(CUSTOM_HEADER, csrf_token).body(r#"<?xml version: "1.0" encoding="UTF-8"?><request><PageIndex>1</PageIndex><ReadCount>20</ReadCount><BoxType>2</BoxType><SortType>0</SortType><Ascending>0</Ascending><UnreadPreferred>0</UnreadPreferred></request>"#).send().unwrap();
+    body = String::from("");
+    res.read_to_string(&mut body);
+    println!("{}", res.status().as_str());
+    println!("{}", body);
 
 }
 
