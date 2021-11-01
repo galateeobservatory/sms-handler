5e6a2ad5d5dc27733f593c2644ba29775152a727
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-11-01 18:27:08 +0100

    Add struct SMS for representing SMS

--- src/e3372.rs
+++ src/e3372.rs
@@ -1,10 +1,14 @@
-use regex::Regex;
-//use reqwest::ClientBuilder;
 use std::io::Read;
 
-pub(crate) struct E3372 {
+use quick_xml::events::Event;
+use quick_xml::Reader;
+use regex::Regex;
+
+pub struct E3372 {
     _base_url: String,
-    _client: reqwest::blocking::Client
+    _client: reqwest::blocking::Client,
+    pub _sent_sms: Vec<SMS>,
+    _receiveid_sms: Vec<SMS>
 }
 
 impl E3372 {
@@ -16,6 +20,8 @@ impl E3372 {
         let new_e3372 = E3372 {
             _base_url: url.clone(),
             _client: builder.build().unwrap(),
+            _sent_sms: vec![],
+            _receiveid_sms: vec![]
         };
         return new_e3372;
     }
@@ -29,13 +35,15 @@ impl E3372 {
         return body;
     }
 
-    pub fn get_sms_list(&self) -> String {
+    pub fn get_sms_list(&mut self, outbox: bool) -> String {
         let csrf_token = E3372::extract_csrf_token(&self.request_cookie_token());
         // boxtype = 1 -> recv
         // boxtype = 2 -> sent
-        let mut res = self._client.post("http://192.168.8.1/api/sms/sms-list").header(E3372::CUSTOM_HEADER, csrf_token.clone()).body(r#"<?xml version: "1.0" encoding="UTF-8"?><request><PageIndex>1</PageIndex><ReadCount>20</ReadCount><BoxType>2</BoxType><SortType>0</SortType><Ascending>0</Ascending><UnreadPreferred>0</UnreadPreferred></request>"#).send().unwrap();
+        let mut res = self._client.post("http://192.168.8.1/api/sms/sms-list").header(E3372::CUSTOM_HEADER, csrf_token.clone()).body(format!("{}{}{}", r#"<?xml version: "1.0" encoding="UTF-8"?><request><PageIndex>1</PageIndex><ReadCount>20</ReadCount><BoxType>"#,
+                                                                                                                                             if outbox {"2"}else{"1"}, r#"</BoxType><SortType>0</SortType><Ascending>0</Ascending><UnreadPreferred>0</UnreadPreferred></request>"#)).send().unwrap();
         let mut body = String::from("");
         res.read_to_string(&mut body).unwrap();
+        self.fill_sms_list(&body, outbox);
         return body;
     }
 
@@ -58,5 +66,55 @@ impl E3372 {
             }
         }
         return String::from(token);
+    }
+
+    fn fill_sms_list(&mut self, xml_resp: &str, outbox: bool) -> () {
+        let mut reader = Reader::from_str(xml_resp);
+        reader.trim_text(true);
+        let mut txt: String = String::new();
+        let mut buf = Vec::new();
+        let mut sms: SMS = SMS { _phone: "".to_string(), _message: "".to_string() };
+        loop {
+            match reader.read_event(&mut buf) {
+                Ok(Event::End(ref e)) => {
+                    match e.name() {
+                        b"Phone" => sms._phone = txt.clone(),
+                        b"Content" => sms._message = txt.clone(),
+                        b"Message" =>
+                            match outbox {
+                                true => self._sent_sms.push(sms.clone()),
+                                false => self._receiveid_sms.push(sms.clone())
+                            },
+                        _ => ()
+                    }
+                },
+                Ok(Event::Text(e)) => txt = e.unescape_and_decode(&reader).unwrap(),
+                Ok(Event::Eof) => break, // exits the loop when reaching end of file
+                Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                _ => (), // There are several other `Event`s we do not consider here
+            }
+
+            // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+            buf.clear();
+        }
+    }
+}
+
+pub struct SMS {
+    pub(crate) _phone: String,
+    pub(crate) _message: String
+}
+
+impl Clone for SMS {
+    fn clone(&self) -> Self {
+        Self {
+            _phone: self._phone.clone(),
+            _message: self._message.clone()
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self._phone = source._phone.clone();
+        self._message = source._message.clone();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod e3372;
+use quick_xml::Reader;
+use quick_xml::events::Event;
 
 fn main() {
-    let e33 = e3372::E3372::new("http://192.168.8.1".parse().unwrap());
-    println!("{}", e33.get_sms_count());
+    let mut e33 = e3372::E3372::new("http://192.168.8.1".parse().unwrap());
+    //println!("{}", e33.get_sms_count());
+    //println!("{}", e33.get_sms_list(true));
+    e33.get_sms_list(true);
+    println!("{}", e33._sent_sms.get(0).unwrap()._phone);
+    for sms in e33._sent_sms.iter() {
+        println!("Phone: {}", sms._phone);
+        println!("Message: {}", sms._message);
+    }
+
+    //let xml = e33.get_sms_count();
 }
